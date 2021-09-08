1fce1a4827b9575c602475cf90c8d1946432ac8c
JingleJenks <sylv2439@stud.kea.dk>
2021-09-08 11:08:03 +0200

    Js added  to main

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const urlParams = new URLSearchParams(window.location.search);
-const subcat = urlParams.get("title");
-const url = "https://keasem2-6806.restdb.io/rest/succulents?max=4";
+// const subcat = urlParams.get("title");
+const url = "https://keasem2-6806.restdb.io/rest/plant?max=4";
 
 const key = {
     headers: {
         "x-apikey": "61361b1443cedb6d1f97ed4f",
     },
 };
-
 
 console.log("hello?");
 fetch(url, key)
@@ -16,19 +15,19 @@ fetch(url, key)
     })
     .then(function (data) {
         // console.log(data);
-        showPlants(data);
+        showNavSpecies(data);
     })
 
-function showPlants(data) {
+function showNavSpecies(data) {
     console.log(data);
-    data.forEach(showTitle);
+    data.forEach(showSpecies);
 }
 
-function showTitle(object) {
-    console.log(object);
-    const template = document.querySelector(".plant").content;
+function showSpecies(obj) {
+    console.log(obj);
+    const template = document.querySelector(".navlist").content;
     const clone = template.cloneNode(true);
-    clone.querySelector("h2").textContent = `$(object.name)`;
+    clone.querySelector("span").textContent = obj.Species;
     // clone.querySelector("img.firstimage").src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
     const parent = document.querySelector("main");
     parent.appendChild(clone);
